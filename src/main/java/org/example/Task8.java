2e9a850500e062491b048916e4933365422f4958
Maiklinfo <maiklinfo@mail.ru>
2024-09-12 15:58:34 +0200

    Задача 8

---
+++ src/main/java/org/example/Task8.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Вводим целое положительное число n
+        System.out.print("Введите целое положительное число n: ");
+        int n = scanner.nextInt();
+
+        // Проверка на положительность числа
+        if (n <= 0) {
+            System.out.println("Ошибка: Введите положительное число.");
+            scanner.close();
+            return;
+        }
+
+        // Инициализация суммы
+        int sum = 0;
+
+        // Суммируем нечетные числа от 1 до n
+        for (int i = 1; i <= n; i += 2) {
+            sum += i;
+        }
+
+        // Выводим результат
+        System.out.println("Сумма всех нечетных чисел от 1 до " + n + " равна: " + sum);
+
+
+        scanner.close();
+    }
+}
