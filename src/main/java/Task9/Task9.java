6316fb1d2f77efc106d41ebe39dc36dcd1ab6127
Maiklinfo <maiklinfo@mail.ru>
2024-09-13 12:11:10 +0200

    Задача 9

---
+++ src/main/java/Task9/Task9.java
@@ -0,0 +1,33 @@
+package Task9;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        double[] array = new double[size];
+
+        System.out.println("Введите " + size + " элементов массива через пробел:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextDouble();
+        }
+
+
+        double sum = 0;
+        for (double num : array) {
+            sum += num;
+        }
+        double average = sum / size;
+
+
+        System.out.println("Массив после умножения каждого элемента на среднее арифметическое:");
+        for (double num : array) {
+            System.out.print(num * average + " ");
+        }
+    }
+}
